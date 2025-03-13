4744fe37e492609fd06a503aa257af078f916ddd
Musonn <mkl5520@psu.edu>
2025-03-12 22:49:58 -0400

    render grid

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,14 @@
 mod game;
-mod tetromino;
 use game::Board;
-use tetromino::{Tetromino, TetrominoType};
 use yew::prelude::*;
-use std::rc::Rc;
-use std::cell::RefCell;
 
 #[function_component(App)]
 fn app() -> Html {
-    let board = use_state(|| Board::new());
-    let active_tetromino = use_state(|| Tetromino::new(TetrominoType::T));
-
-    let move_down = {
-        let tetromino = active_tetromino.clone();
-        Callback::from(move |_| {
-            let mut t = (*tetromino).clone();
-            t.move_down();
-            tetromino.set(t);
-        })
-    };
+    let board = use_state(|| Board::new()); // Create board state
 
     html! {
         <div>
             <h1>{ "Tetris in Rust!" }</h1>
-            <button onclick={move_down}>{ "Move Down" }</button>
             { board.render() }
         </div>
     }
